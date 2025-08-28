bad8eef5909d17ffad477ad4d8533bca85f061d4
Whizlabs <whizlabs@Whizlabss-MacBook-Air.local>
2025-08-28 12:54:45 +0530

    Update server and routes for the deploy

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const router = require("./Routes/appRoutes");
 const db = require("./config/db");
 
@@ -11,7 +11,7 @@ const app = express();
 //     credentials: true,
 //   })
 // );
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
