37254a75c5ae4ec497b0dcc71a101029f0adaf44
Whizlabs <whizlabs@Whizlabss-MacBook-Air.local>
2025-08-28 11:42:43 +0530

    Update TypeORM DB config to use DATABASE_URL and SSL for Render

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,16 @@
-
-
-
 import path from "path";
+import { fileURLToPath } from "url";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const db = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL, 
-  ssl: process.env.DATABASE_URL
-    ? { rejectUnauthorized: false }
-    : false, 
+  url: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
   logging: false,
   synchronize: true,
   entities: [path.join(__dirname, "..", "entities/**/*.js")],
